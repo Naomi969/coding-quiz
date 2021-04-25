2036f55e57bda2be11d7c288abc8dc79678adba4
Naomi Torres <naomitorres@Naomis-MacBook-Air.local>
2021-04-24 23:23:15 -0400

    questions array

--- script.js
+++ script.js
@@ -1,11 +1,64 @@
-function enterQuiz() {
-    console.log("hide")
-    const x = document.getElementById("splashScreen")
-    if (x.style == "block") {
-        console.log("hide")
-        x.style.display = "none"
-    }
-}
+// function enterQuiz() {
+//     console.log("hide")
+//     const x = document.getElementById("splashScreen")
+//     if (x.style == "block") {
+//         console.log("hide")
+//         x.style.display = "none"
+//     }
+// }
 
-const splash = document.getElementById("startButton");
-splash.addEventListener("click", enterQuiz());
+// const splash = document.getElementById("startButton");
+// splash.addEventListener("click", enterQuiz());
+
+var questions [
+    {
+        question1="At the beginning of the series, how many children do Ned and Catelyn Stark have?",
+        options=[
+            '4',
+            '5',
+            '6',
+            '3',
+        ],
+        answer1= 'button1'
+    }
+    {
+        question2="Who is the first character in the series to be called king of the north?",
+        options=[
+            'Robb Stark',
+            'Ned Stark',
+            'Edmure Tully',
+            'Jon Snow',
+        ],
+        answer2= 'button0'
+    }
+    {
+        question3="How does Daenerys hatch her dragon eggs?",
+        options=[
+            'With the help of a witch',
+            'Using wild fire',
+            'In a funeral pyre',
+            'With a lighting strike',
+        ],
+        answer3= 'button2'
+    }
+    {
+        question4="How does Daenerys hatch her dragon eggs?",
+        options=[
+            'Lady',
+            'Ghost',
+            'Grey wind',
+            'Nymeria',
+        ],
+        answer4= 'button3'
+    }
+    {
+        question5="What is Olenna Tyrell's nickname?",
+        options=[
+            'Queen of Hearts',
+            'Queen of Thorns',
+            'Lady of Roses',
+            'Queen of High Garden',
+        ],
+        answer5= 'button1'
+    }
+]
