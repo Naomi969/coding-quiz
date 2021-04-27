6f0e272d468811069287d46cf480394914c0657c
Naomi Torres <naomitorres@Naomis-MacBook-Air.local>
2021-04-26 21:42:59 -0400

    addedlast page

--- Assets/script.js
+++ Assets/script.js
@@ -17,7 +17,9 @@ var incorrentEl = document.getElementById('Incorrect')
 var scoreEl = document.getElementById('scorePage')
 var scoreNum = document.getElementById('score')
 var initialEl = document.getElementById('initial-form')
-
+var scoreBoardEl = document.getElementById('resultsBox')
+var submitEL = document.getElementById('submit')
+var results = []
 
 
 
@@ -25,83 +27,88 @@ var initialEl = document.getElementById('initial-form')
 // this holds all the questions
 var questions = [
     {
-        question:"At the beginning of the series, how many children do Ned and Catelyn Stark have?",
-       
-        btnc0:'4',
-        btnc1:'5',
-        btnc2:'6',
-        btnc3:'3',
-      
+        question: "At the beginning of the series, how many children do Ned and Catelyn Stark have?",
+
+        btnc0: '4',
+        btnc1: '5',
+        btnc2: '6',
+        btnc3: '3',
+
         answer: '5'
     },
     {
-        question:"Who is the first character in the series to be called king of the north?",
-       
-        btnc0:'Robb Stark',
-        btnc1:'Ned Stark',
-        btnc2:'Edmure Tully',
-        btnc3:'Jon Snow',
-      
+        question: "Who is the first character in the series to be called king of the north?",
+
+        btnc0: 'Robb Stark',
+        btnc1: 'Ned Stark',
+        btnc2: 'Edmure Tully',
+        btnc3: 'Jon Snow',
+
         answer: 'Robb Stark'
     },
     {
-        question:"How does Daenerys hatch her dragon eggs?",
-    
-        btnc0:'With the help of a witch',
-        btnc1:'Using wild fire',
-        btnc2:'In a funeral pyre',
-        btnc3:'With a lighting strike',
-       
+        question: "How does Daenerys hatch her dragon eggs?",
+
+        btnc0: 'With the help of a witch',
+        btnc1: 'Using wild fire',
+        btnc2: 'In a funeral pyre',
+        btnc3: 'With a lighting strike',
+
         answer: 'In a funeral pyre'
     },
     {
-        question:"What is the name of Arya Stark's wolf?",
+        question: "What is the name of Arya Stark's wolf?",
 
-        btnc0:'Lady',
-        btnc1:'Ghost',
-        btnc2:'Grey wind',
-        btnc3:'Nymeria',
-        
+        btnc0: 'Lady',
+        btnc1: 'Ghost',
+        btnc2: 'Grey wind',
+        btnc3: 'Nymeria',
+
         answer: 'Nymeria'
     },
     {
-        question:"What is Olenna Tyrell's nickname?",
-       
-        btnc0:'Queen of Hearts',
-        btnc1:'Queen of Thorns',
-        btnc2:'Lady of Roses',
-        btnc3:'Queen of High Garden',
-        
+        question: "What is Olenna Tyrell's nickname?",
+
+        btnc0: 'Queen of Hearts',
+        btnc1: 'Queen of Thorns',
+        btnc2: 'Lady of Roses',
+        btnc3: 'Queen of High Garden',
+
         answer: 'Queen of Thorns'
     }
 ]
 
 //function to populate 
-function showQuestion () {
+function showQuestion() {
     if (cqIndex > questions.length - 1) {
         clearInterval(timer);
         resultsPage();
     }
-    if(questions[cqIndex] !== undefined) {
+    if (questions[cqIndex] !== undefined) {
         splashEl.hidden = true;
         questionsEl.hidden = false;
         buttonsEl.hidden = false;
-        questionsEl.textContent=questions[cqIndex].question;
-        btn0.textContent=questions[cqIndex].btnc0;
-        btn1.textContent=questions[cqIndex].btnc1;
-        btn2.textContent=questions[cqIndex].btnc2;
-        btn3.textContent=questions[cqIndex].btnc3;
+        questionsEl.textContent = questions[cqIndex].question;
+        btn0.textContent = questions[cqIndex].btnc0;
+        btn1.textContent = questions[cqIndex].btnc1;
+        btn2.textContent = questions[cqIndex].btnc2;
+        btn3.textContent = questions[cqIndex].btnc3;
     }
 }
 // function to chech the user choice-- correct/ incorrect are supposed to dissappear when the whole thing starts again.
-function checkSelected(event){
-    if(event.target.textContent === questions[cqIndex].answer){
+function checkSelected(event) {
+    if (event.target.textContent === questions[cqIndex].answer) {
         console.log('correct!!!')
         correctEl.hidden = false
-    }else{
+        incorrentEl.hidden = true
+    } else {
         console.log('incorrect!!!')
         incorrentEl.hidden = false
+        correctEl.hidden = true
         timeLeft = timeLeft - 10
+        if (timeLeft < 0) {
+            resultsPage();
+        }
     }
     cqIndex++
     showQuestion()
@@ -109,36 +116,80 @@ function checkSelected(event){
 
 
 //timer
-function startTimer () {
-    timer = setInterval(function (){
+function startTimer() {
+    timer = setInterval(function () {
         timeLeft--;
         time.textContent = timeLeft + ' seconds';
-    if (timeLeft === 0) {
-        clearInterval (timer);
+        if (timeLeft === 0) {
+            clearInterval(timer);
         }
-    }, 1000);  
+    }, 1000);
 }
 
-startBtn.addEventListener("click", function() {
+function endTimer(timeLeft) {
+    if (timeLeft < 0) {
+        time.className = 'expiredTime'
+    }
+    clearTimeout(timer)
+}
+
+startBtn.addEventListener("click", function () {
     startTimer();
     showQuestion();
 })
 
 //when the quiz is done function
 
-function resultsPage(){
+function resultsPage() {
     console.log('donzo')
     questionsEl.hidden = true
-    buttonsEl.hidden = true 
+    buttonsEl.hidden = true
     correctEl.hidden = true
-    incorrentEl.hidden = true 
+    incorrentEl.hidden = true
     scoreEl.hidden = false
-    initialEl.hidden = false 
+    initialEl.hidden = false
 
-    if (timeLeft < 0){
+    endTimer(timeLeft)
+    if (timeLeft < 0) {
         timeLeft = 0
-    } 
+    }
 
     time.textContent = timeLeft + ' seconds';
     scoreNum.textContent = timeLeft;
+
+
+}
+
+function start() {
+    scoreBoardEl.hidden = true
+}
+
+function saveResult() {
+    var initials = document.getElementById('txtinitials').value;
+     r=   {"initials": initials,
+        "score": scoreNum.innerText}
+        results.push(r)
+    console.log(initials)
+    console.log(scoreNum)
+    console.log(results[0])
+    displayBoard()
+}
+
+submitEL.addEventListener('click', function () { saveResult() })
+
+function displayBoard(){
+    var scorescount = results.length
+
+    for(i=0; i<scorescount; i++)
+    {
+        var cScore = results[i]["initials"]
+        var sInitials = results[i]["score"]
+        var scoreValue = sInitials+" : "+cScore
+        var lblScore = document.createElement('label')
+        var scoreText = document.createTextNode(scoreValue)
+        lblScore.appendChild(scoreText)
+        scoreBoardEl.appendChild(lblScore)
+    }
+    scoreBoardEl.hidden = false;
+
 }
